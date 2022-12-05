a679dc81615f09ac5a468a04331ba5aebe141176
veronika-simic <veronikasimic21@gmail.com>
2022-12-05 08:15:56 +0100

    create nested component

--- udemy_angular/src/app/app.module.ts
+++ udemy_angular/src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
