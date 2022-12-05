f27c002409b0a94b1bd18ad0ce3be95832cbb6db
veronika-simic <veronikasimic21@gmail.com>
2022-12-05 09:05:09 +0100

    data binding

--- udemy_angular/src/app/servers/servers.component.ts
+++ udemy_angular/src/app/servers/servers.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent {
-
+  allowNewServer = false;
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 }
