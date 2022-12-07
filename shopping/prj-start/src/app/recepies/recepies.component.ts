b158a9401d5a11ee6884ce463efbe46e7f9a264b
veronika-simic <veronikasimic21@gmail.com>
2022-12-07 09:37:57 +0100

    output recipe info

--- shopping/prj-start/src/app/recepies/recepies.component.ts
+++ shopping/prj-start/src/app/recepies/recepies.component.ts
@@ -7,13 +7,7 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recepies.component.css'],
 })
 export class RecepiesComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe(
-      'chocolate cake',
-      'best cake ever',
-      'https://images.unsplash.com/photo-1602351447937-745cb720612f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80'
-    ),
-  ];
+  
   constructor() {}
 
   ngOnInit(): void {}
--- shopping/prj-start/src/app/recepies/recipe-list/recipe-list.component.ts
+++ shopping/prj-start/src/app/recepies/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = []
+  recipes: Recipe[] = [
+    new Recipe(
+      'chocolate cake',
+      'best cake ever',
+      'https://images.unsplash.com/photo-1602351447937-745cb720612f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80'
+    ),
+  ];
   constructor() { }
 
   ngOnInit(): void {
