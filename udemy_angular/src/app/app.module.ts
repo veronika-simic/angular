731e3ad468b191d87557b16d46cdc271bdaae060
veronika-simic <veronikasimic21@gmail.com>
2022-12-07 07:21:27 +0100

    two way binding

--- udemy_angular/src/app/app.module.ts
+++ udemy_angular/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, ServerComponent, ServersComponent, WarningAlertComponent, SuccessAlertComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
