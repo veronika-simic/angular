096d056b5879f4a04a5b2956f584510a743fe8f4
veronika-simic <veronikasimic21@gmail.com>
2022-12-07 09:29:46 +0100

    add content to recipe component

--- shopping/prj-start/src/app/recepies/recepies.component.ts
+++ shopping/prj-start/src/app/recepies/recepies.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recepies',
   templateUrl: './recepies.component.html',
-  styleUrls: ['./recepies.component.css']
+  styleUrls: ['./recepies.component.css'],
 })
 export class RecepiesComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'chocolate cake',
+      'best cake ever',
+      'https://images.unsplash.com/photo-1602351447937-745cb720612f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80'
+    ),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
