021da0f240b5d23d9ae71724551a913769154ec1
veronika-simic <veronikasimic21@gmail.com>
2022-12-07 08:44:27 +0100

    create components

--- shopping/prj-start/src/app/app.module.ts
+++ shopping/prj-start/src/app/app.module.ts
@@ -4,10 +4,27 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecepiesComponent } from './recepies/recepies.component';
+import { RecipeListComponent } from './recepies/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recepies/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recepies/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecepiesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+  
   ],
   imports: [
     BrowserModule,
