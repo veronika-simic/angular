8ac9121dbf5a4dfc6068e5f780ad38e6aefe7b8e
veronika-simic <veronikasimic21@gmail.com>
2022-12-08 08:45:47 +0100

    add filter-textbox comp

---
+++ scrimba/customers/src/app/customers/customers-list/filter-textbox.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'app-filter-textbox',
+    template: `
+    Filter: <input type="text"/> 
+    `
+})
+export class FilterTextBoxComponent{
+    constructor(){}
+    ngOnInit(){}
+}
--- scrimba/customers/src/app/customers/customers.module.ts
+++ scrimba/customers/src/app/customers/customers.module.ts
@@ -3,9 +3,10 @@ import {CommonModule } from '@angular/common';
 
 import { CustomersComponent } from './customers.component';
 import { CustomersListComponent } from './customers-list/customers.list.component';
+import { FilterTextBoxComponent } from './customers-list/filter-textbox.component';
 
 @NgModule({
-  declarations: [CustomersComponent, CustomersListComponent],
+  declarations: [CustomersComponent, CustomersListComponent, FilterTextBoxComponent],
   imports: [CommonModule],
   exports: [CustomersComponent]
 })
