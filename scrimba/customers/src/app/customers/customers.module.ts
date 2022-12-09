c66d5e309d34e721bf42beb7d1a5d0cbffa4c332
veronika-simic <veronikasimic21@gmail.com>
2022-12-09 08:22:47 +0100

    pipe for capitalize

--- scrimba/customers/src/app/customers/customers.module.ts
+++ scrimba/customers/src/app/customers/customers.module.ts
@@ -4,10 +4,11 @@ import {CommonModule } from '@angular/common';
 import { CustomersComponent } from './customers.component';
 import { CustomersListComponent } from './customers-list/customers.list.component';
 import { FilterTextBoxComponent } from './customers-list/filter-textbox.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [CustomersComponent, CustomersListComponent, FilterTextBoxComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   exports: [CustomersComponent]
 })
 export class CustomersModule {}
--- scrimba/customers/src/app/shared/shared.module.ts
+++ scrimba/customers/src/app/shared/shared.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
-
+import { CapitalizePipe } from './capitalize.pipe';
 
 @NgModule({
-declarations: [],
-exports: []
+declarations: [CapitalizePipe],
+exports: [CapitalizePipe] /* every module can get it */
 })
 export class SharedModule { }
