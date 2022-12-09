5026a94cb17eb97aabdb9c9fcb10b785ef0116f3
veronika-simic <veronikasimic21@gmail.com>
2022-12-09 08:10:23 +0100

    passing data paren to child

--- scrimba/customers/src/app/customers/customers-list/customers.list.component.ts
+++ scrimba/customers/src/app/customers/customers-list/customers.list.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 import { Customer } from 'src/app/shared/interfaces';
 
 @Component({
   selector: 'app-customers-list',
   templateUrl: './customers.list.component.html',
 })
-export class CustomersListComponent {
+export class CustomersListComponent implements OnInit {
+  private _customers: Customer[] = [];
+  @Input() get customers(): Customer[] {
+    return this._customers
+  }
+
+  set customers(value: Customer[]){
+    if (value){
+      this.filteredCustomers = this._customers = value;
+      this.calculateOrders();
+    }
+  }
+
   filteredCustomers: Customer[] = [];
   customersOrderTotal = 0;
   currency = 'USD';
@@ -15,7 +27,11 @@ export class CustomersListComponent {
   calculateOrders(){
     this.customersOrderTotal = 0;
     this.filteredCustomers.forEach((customer: Customer) => {
-     /*  this.customersOrderTotal += customer.orderTotal; */
-    })
+    /*  this.customersOrderTotal += customer.orderTotal? */
+    });
+  }
+
+  sort(prop: string){
+    /* handles sorting, service */
   }
 }
