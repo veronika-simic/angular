3aaa56a347cd4c894ddff046899e7525f23db6d8
veronika-simic <veronikasimic21@gmail.com>
2022-12-20 08:45:50 +0100

    alias

--- udemy_angular/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ udemy_angular/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -8,6 +8,7 @@ import { Component, Input } from '@angular/core';
 export class ServerElementComponent {
   /* by default not all properties are accessible by other components we have to be explicit about it */
   /* in order to do this we use decorators  */
-  @Input() element: { type: string; content: string; name: string };
+  @Input('srvElement') element: { type: string; content: string; name: string };
   /* now this components property element is accessible to any component that uses it */
+  /* by passing value inside the Input we give it an alias we can use when binding to this prop */
 }
