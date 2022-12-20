138bf673c46c7c1c1a918dad5e74e11ec41033fb
veronika-simic <veronikasimic21@gmail.com>
2022-12-20 09:03:46 +0100

    pass data from child to parent

--- udemy_angular/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ udemy_angular/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,27 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
-  styleUrls: ['./cockpit.component.css']
+  styleUrls: ['./cockpit.component.css'],
 })
 export class CockpitComponent {
+  /* event emmiter is an object that allows us to emit our own events */
+  @Output() serverCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
+  @Output() blueprintCreated = new EventEmitter<{
+    blueprintName: string;
+    blueprintContent: string;
+  }>();
+
   newServerName = '';
   newServerContent = '';
-/* 
+
   onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent,
     });
   }
 
   onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
+    this.blueprintCreated.emit({
+      blueprintName: this.newServerName,
+      blueprintContent: this.newServerContent,
     });
-  } */
+  }
 }
